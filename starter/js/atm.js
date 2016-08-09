97108192792a6e57c25e2935e009ee1a11f07322
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-09 10:05:34 -0500

    insufficent funds

--- starter/js/atm.js
+++ starter/js/atm.js
@@ -6,26 +6,45 @@ $(document).ready(function () {
     var newBalance = currentBalance + value;
     $('#checkingBalance').text(newBalance);
     $('#amountChecking').val('');
+    if (newBalance > 0) {
+      $('#check').css('background-color','#E3E3E3');
+    } else {
+      $('#check').css('background-color','tomato');
+    }
   });
+
   $('#withdrawChecking').on('click', function() {
     var currentBalance = parseInt($('#checkingBalance').text());
     var value = parseInt($('#amountChecking').val());
     var newBalance = currentBalance - value;
     $('#checkingBalance').text(newBalance);
     $('#amountChecking').val('');
+    if (newBalance < 0) {
+      $('#checkingBalance').text(currentBalance);
+      alert('Insufficient Funds')
+    }
   });
+
   $('#depositSavings').on('click', function() {
     var currentBalance = parseInt($('#savingsBalance').text());
     var value = parseInt($('#amountSavings').val());
     var newBalance = currentBalance + value;
     $('#savingsBalance').text(newBalance);
     $('#amountSavings').val('');
+    if (newBalance > 0) {
+      $('#save').css('background-color','#E3E3E3');
+    }
   });
+
   $('#withdrawSavings').on('click', function() {
     var currentBalance = parseInt($('#savingsBalance').text());
     var value = parseInt($('#amountSavings').val());
     var newBalance = currentBalance - value;
     $('#savingsBalance').text(newBalance);
     $('#amountSavings').val('');
+    if (newBalance < 0) {
+      $('#savingsBalance').text(currentBalance);
+      alert('Insufficient Funds')
+    }
   });
 });
